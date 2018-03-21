0a1238adde5e618d36186dcb02e0f3ad85451f6f
Liwen <e0134124@u.nus.edu>
2018-03-21 22:10:29 +0800

    checkstyle

--- src/main/java/seedu/progresschecker/ui/UiManager.java
+++ src/main/java/seedu/progresschecker/ui/UiManager.java
@@ -49,7 +49,7 @@ public class UiManager extends ComponentManager implements Ui {
         logger.info("Starting UI...");
 
         //Set the application icon.
-         primaryStage.getIcons().add(getImage(ICON_APPLICATION));
+        primaryStage.getIcons().add(getImage(ICON_APPLICATION));
 
         try {
             mainWindow = new MainWindow(primaryStage, config, prefs, logic);
